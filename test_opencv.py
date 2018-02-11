6c2dc229543ac65ab497635874274126d054e02a
hv <hvidal@tesseract-tech.com>
2018-02-11 11:04:31 -0500

    HCV: add some more comments

--- test_opencv.py
+++ test_opencv.py
@@ -1,6 +1,21 @@
 # test_opencv.py
+# 11-Feb-2018
 #
-# 
+# author: Hernando Vidal, Jr., Tesseract Tech
+# for American Museum of Natural History 'Hack the Deep' Hackathon
+# 10-11 Feb-2018
+#
+# simple tool to experiment with 'ridge' detection along back of trilobite
+#
+# 1. read in image
+# 2. convert to gray scale
+# 3. apply some gamma correction (currently static value)
+# 4. apply multiple adjacent line profiles along Y in center
+# 5. average line profiles
+# 6. look for trough (minima) peaks
+# 7. draw output data onto lines in output image
+# 8. write out output image
+##################################################################### 
 
 import sys
 
